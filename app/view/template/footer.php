e135d4f54662d7fe2cdf6441274ddf67947e117a
andriand06 <andriandavinta@gmail.com>
2020-04-28 21:13:30 +0700

    menambah bootstrap

--- app/view/template/footer.php
+++ app/view/template/footer.php
@@ -1,2 +1,6 @@
+<script src="<?php echo BASEURL;?>/js/jquery-3.2.1.slim.min.js"></script>
+<script src="<?php echo BASEURL;?>/js/popper.min.js"></script>
+<script src="<?php echo BASEURL;?>/js/bootstrap.min.js"></script>
+
 </body>
 </html>
